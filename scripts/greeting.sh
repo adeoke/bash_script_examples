a93da345e819fd56e14f650e256bbe021b2204b9
adeoke <adesmail100@gmail.com>
2020-11-22 20:41:57 +0000

    improvement

--- scripts/greeting.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Oh, and lines 3 and 4 are comments, might as well slip that in here as well!
-# Lines 3 and 4 will NOT appaer on the output when this file is executed.
-
-echo "Greetings fella!"
